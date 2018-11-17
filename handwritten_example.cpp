29419d5f5341478a5b08d7bc1a8cb5c305a433fe
Daniel Becker <daniel.93.becker@gmail.com>
2018-11-17 12:19:32 +0100

    Last example.

---
+++ handwritten_example.cpp
@@ -0,0 +1,163 @@
+// Computational loop 3 - Average density of each material over neighborhood of each cell
+#if defined(OMP)
+#pragma omp parallel for //collapse(2)
+#elif defined(ACC)
+#pragma acc parallel
+#pragma acc loop independent
+#endif
+for (int j = 1; j < sizey-1; j++) {
+#if defined(OMP)
+#pragma omp simd
+#elif defined(ACC)
+#pragma acc loop independent
+#endif
+    for (int i = 1; i < sizex-1; i++) {
+	// o: outer
+	double xo = ccc.x[i+sizex*j];
+	double yo = ccc.y[i+sizex*j];
+
+	// There are at most 9 neighbours in 2D case.
+	double dsqr[9];
+
+	// for all neighbours
+	for (int nj = -1; nj <= 1; nj++) {
+	    for (int ni = -1; ni <= 1; ni++) {
+
+		dsqr[(nj+1)*3 + (ni+1)] = 0.0;
+
+		// i: inner
+		double xi = ccc.x[(i+ni)+sizex*(j+nj)];
+		double yi = ccc.y[(i+ni)+sizex*(j+nj)];
+
+		dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi);
+		dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi);
+	    }
+	}
+
+	int ix = ccc.imaterial[i+sizex*j];
+
+	if (ix <= 0) {
+	    // condition is 'ix >= 0', this is the equivalent of
+	    // 'until ix < 0' from the paper
+#ifdef LINKED
+	    for (ix = -ix; ix >= 0; ix = ccc.nextfrac[ix]) {
+#else
+		for (int ix = ccc.mmc_index[-ccc.imaterial[i+sizex*j]];
+		     ix < ccc.mmc_index[-ccc.imaterial[i+sizex*j]+1];
+		     ix++) {
+#endif
+
+		    int mat = ccc.matids[ix];
+		    double rho_sum = 0.0;
+		    int Nn = 0;
+
+		    // for all neighbours
+		    for (int nj = -1; nj <= 1; nj++) {
+			for (int ni = -1; ni <= 1; ni++) {
+			    int ci = i+ni, cj = j+nj;
+			    int jx = ccc.imaterial[ci+sizex*cj];
+
+			    if (jx <= 0) {
+				// condition is 'jx >= 0', this is the equivalent of
+				// 'until jx < 0' from the paper
+#ifdef LINKED
+				for (jx = -jx; jx >= 0; jx = ccc.nextfrac[jx]) {
+#else
+				    for (int jx = ccc.mmc_index[-ccc.imaterial[ci+sizex*cj]];
+					 jx < ccc.mmc_index[-ccc.imaterial[ci+sizex*cj]+1];
+					 jx++) {
+#endif
+					if (ccc.matids[jx] == mat) {
+					    rho_sum += ccc.rho_compact_list[jx] /
+						dsqr[(nj+1)*3 + (ni+1)];
+					    Nn += 1;
+
+					    // The loop has an extra condition: "and not found".
+					    // This makes sense, if the material is found,
+					    // there won't be any more of the same.
+					    break;
+					}
+				    }
+				}
+				else {
+				    // NOTE: In this case, the neighbour is a pure cell,
+				    // its material index is in jx.
+				    // NOTE: we index materials from zero,
+				    // but zero can be a list index
+				    int mat_neighbour = jx - 1;
+				    if (mat == mat_neighbour) {
+					rho_sum += ccc.rho_compact[ci+sizex*cj] /
+					    dsqr[(nj+1)*3 + (ni+1)];
+					Nn += 1;
+				    }
+				} // end if (jx <= 0)
+			    } // end for (int ni)
+			} // end for (int nj)
+
+			ccc.rho_mat_ave_compact_list[ix] = rho_sum / Nn;
+		    } // end for (ix = -ix)
+		} // end if (ix <= 0)
+		else {
+		    // NOTE: In this case, the cell is a pure cell, its material index is in ix.
+		    // NOTE: we index materials from zero, but zero can be a list index
+		    int mat = ix - 1;
+
+		    double rho_sum = 0.0;
+		    int Nn = 0;
+
+		    // for all neighbours
+		    for (int nj = -1; nj <= 1; nj++) {
+			if ((j + nj < 0) || (j + nj >= sizey))
+			    continue;
+
+			for (int ni = -1; ni <= 1; ni++) {
+			    if ((i + ni < 0) || (i + ni >= sizex))
+				continue;
+
+			    int ci = i+ni, cj = j+nj;
+			    int jx = ccc.imaterial[ci+sizex*cj];
+
+			    if (jx <= 0) {
+				// condition is 'jx >= 0', this is the equivalent of
+				// 'until jx < 0' from the paper
+#ifdef LINKED
+				for (jx = -jx; jx >= 0; jx = ccc.nextfrac[jx]) {
+#else
+				    for (int jx = ccc.mmc_index[-ccc.imaterial[ci+sizex*cj]];
+					 jx < ccc.mmc_index[-ccc.imaterial[ci+sizex*cj]+1];
+					 jx++) {
+#endif
+					if (ccc.matids[jx] == mat) {
+					    rho_sum += ccc.rho_compact_list[jx] /
+						dsqr[(nj+1)*3 + (ni+1)];
+					    Nn += 1;
+
+					    // The loop has an extra condition: "and not found".
+					    // This makes sense, if the material is found,
+					    // there won't be any more of the same.
+					    break;
+					}
+				    }
+				}
+				else {
+				    // NOTE: In this case, the neighbour is a pure cell,
+				    // its material index is in jx.
+				    // NOTE: we index materials from zero,
+				    // but zero can be a list index
+				    int mat_neighbour = jx - 1;
+				    if (mat == mat_neighbour) {
+					rho_sum += ccc.rho_compact[ci+sizex*cj] /
+					    dsqr[(nj+1)*3 + (ni+1)];
+					Nn += 1;
+				    }
+				} // end if (jx <= 0)
+			    } // end for (int ni)
+			} // end for (int nj)
+
+			ccc.rho_mat_ave_compact[i+sizex*j] = rho_sum / Nn;
+		    } // end else
+		}
+	    }
+	}
+    }
+}
---
+++ mm_example.cpp
@@ -0,0 +1,71 @@
+const std::size_t COLS = ...;
+const std::size_t ROWS = ...;
+
+// Specify which materials are present in each cell.
+const std::vector<std::vector<std::size_t>> materials = ...;
+
+Data<2> data({COLS, ROWS}, materials);
+
+CellData<2> x = data.new_cell_data();
+CellData<2> y = data.new_cell_data();
+CellMataData<2> Vf = data.new_cell_mat_data();
+CellMataData<2> rho = data.new_cell_mat_data();
+CellMataData<2> rho_mat_ave = data.new_cell_mat_data();
+
+// Fill the datasets with data.
+
+void kernel(NeighProxy<CellData<2>> x,
+	    NeighProxy<CellData<2>> y,
+	    NeighProxy<CellMatData<2>> Vf,
+	    NeighProxy<CellMatData<2>> rho,
+	    double &rho_out) {
+    double xo = x[{0,0}];
+    double yo = y[{0,0}];
+
+    double dsqr[9];
+
+    for (int nj = -1; nj <= 1; nj++) {
+	for (int ni = -1; ni <= 1; ni++) {
+
+	    dsqr[(nj+1)*3 + (ni+1)] = 0.0;
+
+	    double xi = x[{ni,nj}];
+	    double yi = y[{ni,nj}];
+
+	    dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi);
+	    dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi);
+	}
+    }
+
+    if (Vf[{0,0}] > 0.0) {
+	double rho_sum = 0.0;
+	int Nn = 0;
+
+	for (int nj = -1; nj <= 1; nj++) {
+	    for (int ni = -1; ni <= 1; ni++) {
+
+		if (Vf[{ni,nj}] > 0.0) {
+		    rho_sum += rho[{ni,nj}] / dsqr[(nj+1)*3 + (ni+1)];
+		    Nn += 1;
+		}
+	    }
+	}
+	rho_out = rho_sum / Nn;
+    } else
+	rho_out = 0.0;
+}
+
+
+Stencil<2> s9pt({{1,1},  {1,0},  {1,-1},
+		 {0,1},  {0,0},  {0,-1},
+		 {-1,1}, {-1,0}, {-1,-1}});
+
+IndexGenerator<2> index_generator({1, 1}, {sizex-1, sizey-1});
+Computation<2> computation(data, index_generator);
+
+computation.compute(kernel,
+		    NEIGH<CellData<2>>(x, s9pt),
+		    NEIGH<CellData<2>>(y, s9pt),
+		    NEIGH<CellMatData<2>>(Vf, s9pt),
+		    NEIGH<CellMatData<2>>(rho, s9pt),
+		    OUT<CellMatData<2>>(rho_mat_ave));
